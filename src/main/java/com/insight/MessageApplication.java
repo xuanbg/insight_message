56724d6d96cdb3b41a0be0890ef399f3eb9f7459
Brian <xan.brian@gmail.com>
2020-06-04 14:52:43 +0800

    up

--- src/main/java/com/insight/MessageApplication.java
+++ src/main/java/com/insight/MessageApplication.java
@@ -3,6 +3,7 @@ package com.insight;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 @EnableAsync
 @EnableScheduling
 @EnableTransactionManagement
